27e97bf7465b14b406cf8f5dfc5e0d12395a9443
Jahanzaib Raza <jraza198@gmail.com>
2024-12-16 00:40:05 +0500

    added redux-persist so we keep our redux store state hydrated even after refreshing the page

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,7 +3,8 @@ import { Home, About, SignIn, SignUp, Profile } from './pages/index.js'
 import { Route, createBrowserRouter, RouterProvider, createRoutesFromElements } from 'react-router-dom'
 
 import { Provider } from 'react-redux'
-import store from './redux/store.js'
+import { persistor, store } from './redux/store.js'
+import { PersistGate } from 'redux-persist/integration/react'
 
 import App from './App.jsx'
 import './index.css'
@@ -23,8 +24,12 @@ const router = createBrowserRouter(
 
 createRoot(document.getElementById('root')).render(
   <Provider store={store}>
-    <RouterProvider router={router} >
-      <App />
-    </RouterProvider>,
+    <PersistGate loading={null} persistor={persistor} >
+      <RouterProvider router={router} >
+
+        <App />
+
+      </RouterProvider>,
+    </PersistGate>
   </Provider>
 )
