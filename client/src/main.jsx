205c56a6efe90494b79a6d96c43f4e99f0932dfb
Jahanzaib Raza <jraza198@gmail.com>
2024-12-15 23:50:31 +0500

    setted up redux store

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,8 +1,12 @@
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
 import { Home, About, SignIn, SignUp, Profile } from './pages/index.js'
-import {Route, createBrowserRouter, RouterProvider, createRoutesFromElements} from 'react-router-dom'
+import { Route, createBrowserRouter, RouterProvider, createRoutesFromElements } from 'react-router-dom'
+
+import { Provider } from 'react-redux'
+import store from './redux/store.js'
+
+import App from './App.jsx'
+import './index.css'
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -12,13 +16,15 @@ const router = createBrowserRouter(
       <Route path='profile' element={<Profile />} />
       <Route path='signin' element={<SignIn />} />
       <Route path='signup' element={<SignUp />} />
-      
+
     </Route>
   )
 )
 
 createRoot(document.getElementById('root')).render(
-  <RouterProvider router={router} >
-    <App />
-  </RouterProvider>,
+  <Provider store={store}>
+    <RouterProvider router={router} >
+      <App />
+    </RouterProvider>,
+  </Provider>
 )
